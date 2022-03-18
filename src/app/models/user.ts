ff8adc99ea2de832aa58cb89a5e5d30d47b75218
Rafael Kastelic <rafaelkastelic@gmail.com>
2022-03-18 00:29:28 -0300

    Adicionando formulario com Angular

---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor(
+        public nome: string= '',
+        public email: string="",
+        public fone: string="",
+        public observacao: string="",
+    ){}
+}
